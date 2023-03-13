f3f0e035464b81afcbe0cffc5cceac3d3ca2c06d
golov <golov@PC-IGORKA>
2023-03-13 20:05:15 +0100

    Добавьте файлы проекта.

---
+++ 10.03/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+namespace _10._03
+{
+    class iPhone
+    {
+        public string Name { get; set; }
+        public string Manufactur { get; set; }
+        public string Price { get; set; }
+        public DateTime ReleaseD { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            iPhone myiPhone = new iPhone();
+            myiPhone.Name = "iPhone 15";
+            myiPhone.Manufactur = "Apple";
+            myiPhone.Price = "1999 $";
+            myiPhone.ReleaseD = new DateTime(2023, 03, 13);
+
+            string json = JsonConvert.SerializeObject(myiPhone);
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ 10.03/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("10.03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ARTLINE")]
+[assembly: AssemblyProduct("10.03")]
+[assembly: AssemblyCopyright("Copyright © ARTLINE 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("669fa11a-38d5-42e2-96d1-2e1d0af80958")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
